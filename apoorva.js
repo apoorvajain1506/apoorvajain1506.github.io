8c2cc133809f4f115560243d6d75ea0754bd09ed
apoorvajain1506 <78037619+apoorvajain1506@users.noreply.github.com>
2021-08-28 19:54:22 +0530

    Update apoorva.js

--- apoorva.js
+++ apoorva.js
@@ -1,31 +1,35 @@
-$(document).ready(function(){
-    $(window).scroll(function()
-    {
-        if(this.scrollY>10)
-        {
-           $(".navbar").addClass("sticky");
-        }
-        else
-        {
-           $(".navbar").removeClass("sticky");
-        }    
-    // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-    //toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    
+AOS.init({
+  duration: 1200,
 });
+
+$(document).ready(function(){
+$(window).scroll(function()
+{
+if(this.scrollY>10)
+{
+ $(".navbar").addClass("sticky");
+}
+else
+{
+ $(".navbar").removeClass("sticky");
+}
+// scroll-up button show/hide script
+if(this.scrollY > 500){
+    $('.scroll-up-btn').addClass("show");
+}else{
+    $('.scroll-up-btn').removeClass("show");
+}
+});
+// slide-up script
+$('.scroll-up-btn').click(function(){
+$('html').animate({scrollTop: 0});
+// removing smooth scroll on slide-up button click
+$('html').css("scrollBehavior", "auto");
+});
+//toggle menu/navbar script
+$('.menu-btn').click(function(){
+$('.navbar .menu').toggleClass("active");
+$('.menu-btn i').toggleClass("active");
+});
+
+})
